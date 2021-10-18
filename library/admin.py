e7f72bc072766a66f255de1eee425c65ba1a0275
Rittwick Bhabak <rittwick2019@gmail.com>
2021-10-18 23:09:39 +0530

    admin title added

--- library/admin.py
+++ library/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Book 
-
+admin.site.site_header = 'Library Admin'
+admin.site.site_title = 'Library Admin Area'
+admin.site.index_title = 'Welcome to the Library admin area'
 admin.site.register(Book)
